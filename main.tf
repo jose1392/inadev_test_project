11d332aa123f2a0e035def91dd8433bf9d4f8cb9
jose1392 <150645133+jose1392@users.noreply.github.com>
2024-04-20 14:06:01 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_route_table_association" "public_subnet_association" {
 # EKS Cluster with Public Access 
 resource "aws_eks_cluster" "eks_cluster" {
   name          = "my-eks-cluster"
-  role_arn      = "arn:aws:iam::ACCOUNT_ID:role/eks-admin-role" # Replace with your IAM role ARN
+  role_arn      = "arn:aws:iam::ACCOUNT_ID:role/eks-admin-role" 
   vpc_config {
     security_group_ids = ["${aws_security_group.eks_sg.id}"]
     subnet_ids        = [aws_subnet.public_subnet.id]
